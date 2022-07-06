e4972a82793a42a260732d7a6b790204bace489b
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-07 00:14:52 +0900

    [add] AC: https://atcoder.jp/contests/abs/submissions/33024746

--- abs/abc085b/main.go
+++ abs/abc085b/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
+type MochiSet map[int]struct{}
+
+// 1行読み込んで MochiSet にする
+func readLineToMochiSet(lineNumber int) MochiSet {
+	ms := make(MochiSet, 100)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines) // 行区切りで読み込む
+
+	for lc := 0; lc < lineNumber; lc++ {
+		for scanner.Scan() {
+			i, _ := strconv.Atoi(scanner.Text())
+			ms[i] = struct{}{}
+		}
+	}
+	return ms
+}
+
 func main() {
-	fmt.Println(0)
+	var n int
+	fmt.Scan(&n)
+	ms := readLineToMochiSet(n)
+	fmt.Println(len(ms))
 }
