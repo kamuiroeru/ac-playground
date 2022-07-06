54d46e62c731fdf431e01dd769178c705e42c345
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-06 23:45:40 +0900

    [add] AC: https://atcoder.jp/contests/abs/submissions/33024171

--- abs/abc083b/main.go
+++ abs/abc083b/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println(0)
+	var n, a, b int
+	fmt.Scan(&n)
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+
+	sum := 0
+	for i := 1; i < n+1; i++ {
+		digitSum := 0
+		for _, digitStr := range strings.Split(strconv.Itoa(i), "") {
+			digitInt, _ := strconv.Atoi(digitStr)
+			digitSum += digitInt
+		}
+		if a <= digitSum && digitSum <= b {
+			sum += i
+		}
+	}
+
+	fmt.Println(sum)
 }
