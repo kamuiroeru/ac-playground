baed556d4f4be6c37e0a94ab9884b5eac432ece5
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-06 23:30:09 +0900

    [add] AC: https://atcoder.jp/contests/abs/submissions/33023764

--- abs/abc087b/main.go
+++ abs/abc087b/main.go
@@ -5,5 +5,42 @@ import (
 )
 
 func main() {
-	fmt.Println(0)
+	var a, b, c, x int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&x)
+
+	pattern := 0
+	i := a
+	for i >= 0 {
+		if i*500 > x {
+			i--
+			continue
+		}
+		j := b
+		for j >= 0 {
+			if i*500+j*100 > x {
+				j--
+				continue
+			}
+			k := c
+			for k >= 0 {
+				// fmt.Println(i, j, k, pattern)
+				if i*500+j*100+k*50 > x {
+					k--
+					continue
+				}
+				if i*500+j*100+k*50 == x {
+					pattern++
+					break
+				}
+				k--
+			}
+			j--
+		}
+		i--
+	}
+
+	fmt.Println(pattern)
 }
