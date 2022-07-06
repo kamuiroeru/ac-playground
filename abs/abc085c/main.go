ba4dec08390c4b3d9550cfd62e6b99406c1fe9da
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-07 01:16:44 +0900

    [add] AC: https://atcoder.jp/contests/abs/submissions/33025779

--- abs/abc085c/main.go
+++ abs/abc085c/main.go
@@ -5,5 +5,30 @@ import (
 )
 
 func main() {
-	fmt.Println(0)
+	var N, Y int
+	fmt.Scan(&N)
+	fmt.Scan(&Y)
+
+	// 3元2連立方程式なので、 x を定数だと考えて(2001回)、2元2連立方程式を解く
+	for x := 0; x < 2000+1; x++ {
+		fourY := Y/1000 - N - 9*x
+		fourZ := -Y/1000 + 5*N + 5*x
+		// fmt.Println(fourY, fourZ)
+		if fourY == 0 {
+			fmt.Println(x, 0, N-x)
+			return
+		}
+		if fourZ == 0 {
+			fmt.Println(x, N-x, 0)
+			return
+		}
+		if fourY > 0 && fourZ > 0 {
+			if fourY%4 == 0 && fourZ%4 == 0 {
+				fmt.Println(x, fourY/4, fourZ/4)
+				return
+			}
+		}
+	}
+
+	fmt.Println(-1, -1, -1)
 }
