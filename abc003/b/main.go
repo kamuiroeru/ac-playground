f4bdc8c74f6d4422245c61922d18b89da9db5014
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-11 23:50:24 +0900

    [add] AC: https://atcoder.jp/contests/abc003/submissions/33170679

--- abc003/b/main.go
+++ abc003/b/main.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+const replaceable = "atcoder"
+
 func main() {
-	fmt.Println(0)
+	var S, T string
+	fmt.Scan(&S)
+	fmt.Scan(&T)
+
+	for i := 0; i < len(S); i++ {
+
+		if S[i:i+1] == T[i:i+1] {
+			continue
+		}
+
+		if S[i:i+1] == "@" {
+			if !strings.Contains(replaceable, T[i:i+1]) {
+				fmt.Println("You will lose")
+				return
+			}
+			continue
+		}
+
+		if T[i:i+1] == "@" {
+			if !strings.Contains(replaceable, S[i:i+1]) {
+				fmt.Println("You will lose")
+				return
+			}
+			continue
+		}
+
+		if S[i:i+1] != T[i:i+1] {
+			fmt.Println("You will lose")
+			return
+		}
+	}
+
+	fmt.Println("You can win")
 }
