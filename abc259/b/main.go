edb4219d74461463c937ec8be66344ecfeaa20b7
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-13 22:16:03 +0900

    [add] AC: https://atcoder.jp/contests/abc259/submissions/33206462

--- abc259/b/main.go
+++ abc259/b/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println(0)
+	var a, b, d float64
+	fmt.Scanf("%f %f %f", &a, &b, &d)
+	// fmt.Println(a, b, d)
+	dRad := d * math.Pi / 180.0
+	retA := a*math.Cos(dRad) - b*math.Sin(dRad)
+	retB := a*math.Sin(dRad) + b*math.Cos(dRad)
+	fmt.Println(retA, retB)
 }
