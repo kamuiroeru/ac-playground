2e0ad488f9e49a470fbeee338a4095136211aa5e
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2022-07-20 21:08:57 +0900

    [add] AC: https://atcoder.jp/contests/abc260/submissions/33379850

--- abc260/a/main.go
+++ abc260/a/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println(0)
+	var S string
+	fmt.Scan(&S)
+	a := S[0:1]
+	b := S[1:2]
+	c := S[2:3]
+
+	if a == b {
+		if b == c {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(c)
+		}
+	} else {
+		if a == c {
+			fmt.Println(b)
+		} else {
+			fmt.Println(a)
+		}
+	}
 }
